77cb30723ddcb336a38bd0db6357038fbef2399f
FambaJava <akim74@web.de>
2020-02-26 10:45:01 +0100

    SQL is now working!

--- Master/src/main/java/cloudsystem/database/SQLManager.java
+++ Master/src/main/java/cloudsystem/database/SQLManager.java
@@ -7,7 +7,7 @@ import java.sql.*;
 
 public class SQLManager {
 
-    private String  user, password;
+    private String user, password;
 
     private Connection connection;
 
@@ -17,48 +17,56 @@ public class SQLManager {
     }
 
     @lombok.SneakyThrows
-    public void openConnection(){
+    public void openConnection() {
         Class.forName("org.h2.Driver");
         System.out.println("Connecting to DB...");
-        connection = DriverManager.getConnection("jdbc:h2:tcp://localhost/~/test ", user, password);
+        connection = DriverManager.getConnection("jdbc:h2:tcp://localhost/~/test", user, password);
         System.out.println("Connected");
         Statement tableStatement = connection.createStatement();
-        String sql = "CREATE TABLE IF NOT EXISTS SERVERS(\n" + "Type VARCHAR(100), \n" + "Name VARCHAR(100), \n" + "Id int(100), \n" + "IpAdress VARCHAR(100), \n" + "Port INT(100), \n" + "Password VARCHAR(100)\n" + ");";
-        sql = "CREATE TABLE IF NOT EXISTS `sun_channel` (\n" +
-                "    `ID` int(11) unsigned NOT NULL auto_increment,\n" +
-                "    `EMAIL` varchar(255) NOT NULL default '',\n" +
-                "    `PASSWORD` varchar(255) NOT NULL default '',\n" +
-                "    `PERMISSION_LEVEL` tinyint(1) unsigned NOT NULL default '1',\n" +
-                "    `APPLICATION_COMPLETED` boolean NOT NULL default '0',\n" +
-                "    `APPLICATION_IN_PROGRESS` boolean NOT NULL default '0',\n" +
-                "    PRIMARY KEY  (`ID`)\n" +
-                ")";
+        String sql = "CREATE TABLE IF NOT EXISTS `ONLINE_SERVERS`\n" +
+                "(`TYPE` VARCHAR(100),\n" +
+                "`NAME` VARCHAR(100), \n" +
+                "`ID` INTEGER(100),\n" +
+                "`ADRESS` VARCHAR(100), \n" +
+                "`PORT` INTEGER(100), \n" +
+                "`PASSWORD` VARCHAR(100))";
         tableStatement.executeUpdate(sql);
         System.out.println("Created table");
     }
 
     public boolean setUpTeamSpeak(String name, int id, String ipAdress, int port, String password) {
-        try{
+        try {
+            System.out.println("SetUp new teamspeak.");
             Statement tableStatement = connection.createStatement();
-            String sql = "INSERT INTO SERVERS (Type, Name, Id, IpAdress, Port, Password) \n" +
-                    "VALUES (TeamSpeak, " + name + ", " + id + ", " + ipAdress + ", " + password + ", " + password + ";";
-
+            String sql = "INSERT INTO ONLINE_SERVERS (TYPE, NAME, ID, ADRESS, PORT, PASSWORD)\n" +
+                    "VALUES ('TeamSpeak', '" + name + "', " + id + ", '" + ipAdress + "', " + port + "  , '" + password + "')";
             tableStatement.executeUpdate(sql);
+            System.out.println("setup finished");
             return true;
-        }catch (Exception ex){
+        } catch (Exception ex) {
+            System.out.println("setup finished with errors: " + ex.getMessage());
             return false;
         }
     }
 
     public TeamSpeakServer getTeamSpeakServer(int id) throws SQLException {
+        System.out.println("create statement");
         Statement statement = connection.createStatement();
-        ResultSet rs;
-        String sql = "SELECT * FROM SERVERS WHERE Id = " + id + ";";
+        ResultSet rs ;
+        String sql = "SELECT * FROM ONLINE_SERVERS WHERE ID = " + id + ";";
+        System.out.println("execute query");
         rs = statement.executeQuery(sql);
-        String name = rs.getString("Name");
-        String ipAdress = rs.getString("IpAdress");
-        int port = rs.getInt("Port");
-        String password = rs.getString("Password");
+        String name = null;
+        String ipAdress = null;
+        int port = 0;
+        String password = null;
+        while (rs.next()){
+            name = rs.getString("NAME");
+            ipAdress = rs.getString("ADRESS");
+            port = rs.getInt("PORT");
+            password = rs.getString("PASSWORD");
+        }
+        System.out.println(name);
         return new TeamSpeakServer(name, id, ipAdress, port, password);
     }
 
